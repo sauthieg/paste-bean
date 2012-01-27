24017ba84cd77e2cc32e1212d753c2775f8d610c
Guillaume Sauthier <guillaume.sauthier@gmail.com>
2012-01-27 23:59:31 +0100

    Add a filter on jspx pages to include the list of the latest Pastes

---
+++ pastebean-web/src/main/java/org/ow2/jonas/azure/pastebean/control/LatestPastesFilter.java
@@ -0,0 +1,43 @@
+package org.ow2.jonas.azure.pastebean.control;
+
+import org.ow2.jonas.azure.pastebean.model.Paste;
+import org.ow2.jonas.azure.pastebean.service.PasteService;
+
+import javax.ejb.EJB;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: guillaume
+ * Date: 27/01/12
+ * Time: 23:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class LatestPastesFilter implements Filter {
+    @EJB
+    private PasteService pasteService;
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+        System.out.println("latest");
+        List<Paste> latest = pasteService.findLatestPastes(10);
+        System.out.println("latests: " + latest.size());
+        req.setAttribute("latest", latest);
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
