92dabcbcbee88e2868f74fd2efae588aabe2cf29
Guillaume Sauthier <guillaume.sauthier@ow2.org>
2012-01-27 18:01:15 +0100

    Move to JSPX

---
+++ pastebean-web/src/main/java/org/ow2/jonas/azure/pastebean/control/ShowPasteControllerServlet.java
@@ -0,0 +1,58 @@
+package org.ow2.jonas.azure.pastebean.control;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ow2.jonas.azure.pastebean.model.Paste;
+import org.ow2.jonas.azure.pastebean.service.PasteService;
+
+/**
+ * A {@code ${NAME}} is ...
+ *
+ * @author Guillaume Sauthier
+ */
+public class ShowPasteControllerServlet extends HttpServlet {
+
+    @EJB
+    private PasteService pasteService;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        execute(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        execute(request, response);
+    }
+
+    private void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Remove the leading '/'
+        String hashFragment = request.getPathInfo().substring(1);
+        System.out.println("PathInfo: " + request.getPathInfo());
+        System.out.println("Hash: " + hashFragment);
+
+        List<Paste> pastes = pasteService.findMatchingPastes(hashFragment);
+        
+        if (pastes.isEmpty()) {
+            // forward to error page
+            System.out.println("Empty result: Forward to error page");
+        } else if (pastes.size() > 1) {
+            // forward to error page
+            System.out.println("Multiple matches: Forward to error page");
+        } else {
+            // Only 1 match
+            request.setAttribute("paste", pastes.get(0));
+            RequestDispatcher dispatcher = request.getRequestDispatcher("paste.jspx");
+            dispatcher.forward(request, response);
+        }
+    }
+
+
+}
